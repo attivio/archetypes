f10e9d94497f2829b2d493935a44ff5c6d5f3f6a
martin <martin@attivio.com>
2017-03-14 10:59:23 -0400

    PLAT-39076, added client archetype example

---
+++ attivio-archetype-client/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,59 @@
+def moduleDir = new File(request.getOutputDirectory()+"/"+request.getArtifactId())
+
+// Find Attivio installation
+def env = System.getenv()
+def attivioHome = env['ATTIVIO_HOME']
+if (attivioHome == null) {
+    env['PATH'].split(System.getProperty("path.separator")).each { p ->
+	if (new File(p+"/../conf/attivio.license").exists()) {
+	    attivioHome = new File(p).getParent();
+	}
+    }
+}
+if (attivioHome == null) {
+    println "Client requires Attivio installation and Attivio was not found on path"
+    attivioHome = System.console().readLine 'Attivio Installation Directory?: '
+}
+if (attivioHome != null) {
+    println "Attivio installed at: ${attivioHome}"
+}
+
+// handle inclusion of hadoop dependencies
+def hadoopMarker = 'HADOOP-MARKER'
+def pomFile = new File(moduleDir, 'pom.xml')
+println "Updating "+pomFile
+
+def pomContent = pomFile.getText('UTF-8')
+def hadoopDependencies = """
+    <!-- Hadoop runtime dependencies -->
+    <dependency>
+      <groupId>org.apache.hbase</groupId>
+      <artifactId>hbase-client</artifactId>
+      <version>1.1.2</version>
+    </dependency>
+    <dependency>
+      <groupId>org.apache.hadoop</groupId>
+      <artifactId>hadoop-hdfs</artifactId>
+      <version>2.7.2</version>
+    </dependency>
+    <dependency>
+      <groupId>org.apache.hadoop</groupId>
+      <artifactId>hadoop-common</artifactId>
+      <version>2.7.2</version>
+    </dependency>
+"""
+if (hadoop.equalsIgnoreCase("yes") || hadoop.equalsIgnoreCase("y")) {
+    // replace marker with dependencies
+    pomContent = pomContent.replace(hadoopMarker, hadoopDependencies)
+} else {
+    // remove marker from pom
+    pomContent = pomContent.replace(hadoopMarker, '')
+}
+
+// update home directory
+pomContent = pomContent.replace('ATTIVIO-HOME', attivioHome);
+
+// rewrite pom.xml
+pomFile.newWriter().withWriter { w ->
+    w << pomContent
+}
